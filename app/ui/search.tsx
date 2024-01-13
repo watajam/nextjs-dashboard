8f9448cc9f24b75de37ab394a213f972824dde23
watajam <wata124coron@icloud.com>
2024-01-13 19:54:14 +0900

    Refactor dashboard page and add pagination to invoices page

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,31 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useQueryState } from 'nuqs';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const [query, setQuery] = useQueryState('query', {
+    shallow: false,
+    throttleMs: 1000,
+  });
+  const [page, setPage] = useQueryState('page', {
+    shallow: false,
+  });
+  // const searchParams = useSearchParams();
+  // const pathname = usePathname();
+  // const { replace } = useRouter();
+  // function handleSearch(term: string) {
+  //   console.log(searchParams);
+
+  //   const params = new URLSearchParams(searchParams);
+  //   if (term) {
+  //     params.set('query', term);
+  //   } else {
+  //     params.delete('query');
+  //   }
+  //   replace(`${pathname}?${params.toString()}`);
+  // }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +34,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        // onChange={(e) => {
+        //   handleSearch(e.target.value);
+        // }}
+        onChange={(e) => {
+          setPage('1');
+          setQuery(e.target.value);
+        }}
+        // defaultValue={searchParams.get('query')?.toString()}
+        value={query || ''}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
